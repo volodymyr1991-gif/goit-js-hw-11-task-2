f69ef37189ee911590d39c93e7d3c80b8748c1b9
Volodymyr Molodynia <molodunja91@gmail.com>
2020-10-05 02:47:04 +0300

    add task2

---
+++ task.js
@@ -0,0 +1,58 @@
+const refs = {
+  days: document.querySelector('span[data-value=days]'),
+  hours: document.querySelector('span[data-value=hours]'),
+  minutes: document.querySelector('span[data-value=mins]'),
+  seconds: document.querySelector('span[data-value=secs]'),
+  startBtn: document.querySelector('button[data-action=start]'),
+  stopBtn: document.querySelector('button[data-action=stop]'),
+};
+
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    const startTime = new Date('Jul 17, 2021');
+
+    updateClockFace(0);
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+
+      updateClockFace(deltaTime);
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    this.isActive = false;
+    updateClockFace(0);
+  },
+};
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
+
+function updateClockFace(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${mins}`;
+  refs.seconds.textContent = `${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
